9337e82bc9c918017fc4118d84d447b4bdb3d4be
sanchoponcho08 <kevinsito1559@gmail.com>
2025-09-17 22:18:32 -0300

    login conexion bd

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,6 +1,7 @@
 using InmobiliariaWebApp.Data; 
-using InmobiliariaWebApp.Models; 
+using InmobiliariaWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace InmobiliariaWebApp.Controllers
 {
@@ -8,7 +9,7 @@ namespace InmobiliariaWebApp.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        
+
         public UsuarioController(ApplicationDbContext context)
         {
             _context = context;
@@ -26,13 +27,43 @@ namespace InmobiliariaWebApp.Controllers
         {
             // Asignamos un rol por defecto
             usuario.Rol = "Empleado";
-    
+
             // Guardamos el nuevo usuario en la base de datos
             _context.Add(usuario);
             await _context.SaveChangesAsync();
-        
+
             // Redirigimos al usuario a la página de login
             return RedirectToAction("Login");
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Login(string Email, string Clave)
+        {
+    // Buscamos el usuario en la base de datos por su email
+    var usuario = await _context.Usuarios
+        .FirstOrDefaultAsync(u => u.Email == Email);
+
+    if (usuario == null)
+    {
+        // Si no se encuentra el usuario, volvemos a la vista de login
+        //  agregar un mensaje de error despues
+        return View();
+    }
+
+    // Comparamos la contraseña del formulario con la guardada en la BD texto luego modificar
+    if (usuario.Clave != Clave)
+    {
+        
+        return View();
+    }
+
+    
+    return RedirectToAction("Index", "Home");
+}
+        
     }
 }
