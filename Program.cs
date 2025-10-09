39e47f95a484dd246872bd7a93d337b10dbcb2b1
Ian Chiti <ianchiti308@gmail.com>
2025-10-08 23:22:15 -0300

    cambios a inquilinos
    LoginPartial add :)

--- Program.cs
+++ Program.cs
@@ -1,37 +1,46 @@
-using InmobiliariaWebApp.Data;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Configurar la autenticación con cookies
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
         options.LoginPath = "/Usuario/Login";
-        options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+        options.LogoutPath = "/Usuario/Logout";
         options.AccessDeniedPath = "/Home/AccessDenied";
     });
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), 
-    new MySqlServerVersion(new Version(8, 0, 21)))); 
+// Configuración de la sesión
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
-
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization(); 
 
+app.UseSession();
+
+// Usar autenticación y autorización
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
