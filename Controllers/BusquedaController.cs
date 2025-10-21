dce863ef1c88ed962909bbf4a85b94235f595b32
ianchiti308 <ianchiti308@gmail.com>
2025-10-21 19:24:15 +0000

    fixes

--- Controllers/BusquedaController.cs
+++ Controllers/BusquedaController.cs
@@ -1,69 +1,71 @@
 using InmobiliariaWebApp.Models;
+using InmobiliariaWebApp.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 
 namespace InmobiliariaWebApp.Controllers
 {
     [Authorize]
     public class BusquedaController : Controller
     {
-        // private readonly ApplicationDbContext _context;
+        private readonly IInmuebleRepository _inmuebleRepository;
+        private readonly IContratoRepository _contratoRepository;
 
-        // public BusquedaController(ApplicationDbContext context)
-        // {
-        //     _context = context;
-        // }
+        public BusquedaController(IInmuebleRepository inmuebleRepository, IContratoRepository contratoRepository)
+        {
+            _inmuebleRepository = inmuebleRepository;
+            _contratoRepository = contratoRepository;
+        }
 
         public IActionResult Index()
         {
-            // ViewData["TipoInmuebleId"] = new SelectList(_context.TiposInmuebles, "Id", "Nombre");
+            ViewData["TipoInmuebleId"] = new SelectList(_inmuebleRepository.GetTiposInmueble(), "Id", "Nombre");
             return View();
         }
 
         [HttpPost]
         public IActionResult Buscar(DateTime fechaInicio, DateTime fechaFin, int tipoInmuebleId, decimal precioMin, decimal precioMax)
         {
-            // // inmuebles ocupados en esas fechas
-            // var inmueblesOcupadosIds = _context.Contratos
-            //     .Where(c => (fechaInicio < c.FechaFin && fechaFin > c.FechaInicio))
-            //     .Select(c => c.InmuebleId)
-            //     .Distinct()
-            //     .ToList();
+            // inmuebles ocupados en esas fechas
+            var todosLosContratos = _contratoRepository.GetAll();
+            var inmueblesOcupadosIds = todosLosContratos
+                .Where(c => (fechaInicio < c.FechaFin && fechaFin > c.FechaInicio))
+                .Select(c => c.InmuebleId)
+                .Distinct()
+                .ToList();
 
-            // var consulta = _context.Inmuebles
-            //     .Include(i => i.Dueño)
-            //     .Include(i => i.Tipo)
-            //     .Where(i => i.Disponible); // Solo inmuebles disponibles
+            var todosLosInmuebles = _inmuebleRepository.GetAll();
+            var consulta = todosLosInmuebles
+                .Where(i => i.Disponible); // Solo inmuebles disponibles
 
-            // // Filtro No MOSTRAR ocupados
-            // if (inmueblesOcupadosIds.Any())
-            // {
-            //     consulta = consulta.Where(i => !inmueblesOcupadosIds.Contains(i.Id));
-            // }
+            // Filtro No MOSTRAR ocupados
+            if (inmueblesOcupadosIds.Any())
+            {
+                consulta = consulta.Where(i => !inmueblesOcupadosIds.Contains(i.Id));
+            }
 
-            // // Filtramos x tipoInmuble
-            // if (tipoInmuebleId > 0)
-            // {
-            //     consulta = consulta.Where(i => i.TipoInmuebleId == tipoInmuebleId);
-            // }
+            // Filtramos x tipoInmuble
+            if (tipoInmuebleId > 0)
+            {
+                consulta = consulta.Where(i => i.TipoInmuebleId == tipoInmuebleId);
+            }
 
-            // // Filtro x precio
-            // if (precioMin > 0)
-            // {
-            //     consulta = consulta.Where(i => i.Precio >= precioMin);
-            // }
-            // if (precioMax > 0 && precioMax > precioMin)
-            // {
-            //     consulta = consulta.Where(i => i.Precio <= precioMax);
-            // }
+            // Filtro x precio
+            if (precioMin > 0)
+            {
+                consulta = consulta.Where(i => i.Precio >= precioMin);
+            }
+            if (precioMax > 0 && precioMax > precioMin)
+            {
+                consulta = consulta.Where(i => i.Precio <= precioMax);
+            }
 
-            // var inmueblesDisponibles = consulta.ToList();
+            var inmueblesDisponibles = consulta.ToList();
             
-            // ViewData["TipoInmuebleId"] = new SelectList(_context.TiposInmuebles, "Id", "Nombre", tipoInmuebleId);
+            ViewData["TipoInmuebleId"] = new SelectList(_inmuebleRepository.GetTiposInmueble(), "Id", "Nombre", tipoInmuebleId);
 
-            return View("Index", new List<Inmueble>());
+            return View("Index", inmueblesDisponibles);
         }
     }
 }
